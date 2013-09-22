7f61be70076b39407bb29c056b2511d202457b52
madsailor <drbluesman@yahoo.com>
2013-09-22 15:29:31 -0700

    Bugfix  MORNINGFIN

--- src/morningstar.py
+++ src/morningstar.py
@@ -42,56 +42,71 @@ def find_exchange(symbol):
     exchange = ystockquote.get_stock_exchange(symbol)
     if exchange == '"AMEX"':
         exchange = 'XASE'
+        return exchange
     elif exchange =='"NasdaqNM"':
         exchange = 'XNAS'
+        return exchange
     elif exchange =='"NYSE"':
         exchange = 'XNYS'
-    return exchange
+        return exchange
+    else:
+        return 'Ticker Not Supported'
 
 def fetch_keyratios(symbol, datacode):
+    if datacode < 1 or datacode > 1115 :
+        return 'Invalid Datacode'
+    #query remote and catch errors
     exchange = find_exchange(symbol)
-    #catch errors
+    if exchange == 'Ticker Not Supported':
+        return exchange
     keyratio_dict = query_morningstar(exchange, symbol,'&region=usa&culture=en-US&cur=USD&order=desc')
     if keyratio_dict == 'Check Connection' or keyratio_dict == 'Not Available':
-        return keyratio_dict    
-    elif datacode < 1 or datacode > 1115 :
-        return 'Invalid Datacode'
+        return keyratio_dict
+    #iterate through returned dict line by line    
     counter = 0
     skipped = 0            
     for line in keyratio_dict:
-    #match year values to datacodes
-        for val in range(1, len(line)):
-            if datacode == val:
-                return keyratio_dict.fieldnames[val]
-    #match data values to datacodes
         if counter == 15 or counter == 16 or counter == 27 or counter == 37 or counter == 38 or counter == 60 or counter == 68 or counter == 91 or counter == 97:
-            skipped += 1
             counter += 1
-        for val in range(1, len(line)):
-            if (datacode - (counter * len(line) - skipped * len(line)) +(counter+ 1)) - len(line) == val:
-                data = line[keyratio_dict.fieldnames[val]]
-                return data
+            skipped += 1
+        else:
+            for val in range(1, len(line)):
+                #match year values to datacodes
+                if datacode == val:
+                    return keyratio_dict.fieldnames[val]
+                #match data values to datacodes
+                if (datacode - (counter * len(line) - skipped * len(line)) +(counter+ 1)) - len(line) == val:
+                    data = line[keyratio_dict.fieldnames[val]]
+                    return data
         counter += 1
     return 'No Data'
 
 def fetch_financials(symbol, datacode):
+    if datacode < 1 or datacode > 120 :
+        return 'Invalid Datacode'
+    #query remote and catch errors
     exchange = find_exchange(symbol)
-    #catch errors
+    if exchange == 'Ticker Not Supported':
+        return exchange
     financial_dict = query_morningstar(exchange, symbol,'&region=usa&culture=en-US&cur=USD&reportType=is&period=12&dataType=A&order=desc&columnYear=5&rounding=3&view=raw&r=113199&denominatorView=raw&number=3')
     if financial_dict == 'Check Connection' or financial_dict == 'Not Available':
-        return financial_dict    
-    if datacode < 1 or datacode > 138 :
-        return 'Invalid Datacode' 
+        return financial_dict     
     counter = 0
+    skipped = 0
+    #iterate through returned dict line by line
     for line in financial_dict:
-        for val in range(1, len(line)):
-            #match year values to datacodes
-            if datacode == val:
-                return financial_dict.fieldnames[val]
-            #match data values to datacodes
+        #skip lines with no data
+        if counter == 3 or counter == 16 or counter == 19:
+            skipped += 1
+        else:
+            for val in range(1, len(line)):
+                #match year values to datacodes
+                if datacode == val:
+                    return financial_dict.fieldnames[val]
+                #match data values to datacodes
 #todo: doesn't read first line 'Revenue'
-            if datacode - counter * (len(line)-1) == val:
-                data = line[financial_dict.fieldnames[val]]
-                return data
+                if datacode - (counter - skipped) * (len(line)-1) == val:
+                    data = line[financial_dict.fieldnames[val]]
+                    return data
         counter += 1
     return 'No Data'
